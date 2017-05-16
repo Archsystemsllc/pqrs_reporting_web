9883ff98e344d8cee8b2590340e717af77d9f1e1
MurugarajKandaswam <MurugarajKandaswam@10.1.10.250>
2017-05-16 12:50:23 -0400

    Initial Version

---
+++ src/main/java/com/archsystemsinc/pqrs/model/Pchpsa.java
@@ -0,0 +1,35 @@
+package com.archsystemsinc.pqrs.model;
+
+//import org.springframework.data.annotation.Id;
+//import org.springframework.data.mongodb.core.mapping.Document;
+
+/**
+ * Created by MurugarajKandaswam on 5/11/2017.
+ */
+//@Document(collection = "pchpsa")
+public class Pchpsa {
+
+  //  @Id
+    private String id;
+
+    String zipCode;
+    String state;
+
+    public Pchpsa(String zipCode, String state) {this.zipCode = zipCode; this.state = state;}
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+}
